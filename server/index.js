c3a8d82fe2ed559641ee0af2b4370562eba4f6af
AmineCoder30 <99543252+AmineCoder30@users.noreply.github.com>
2024-09-25 13:51:53 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const server = createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST"],
+   methods: ["GET", "POST", "DELETE", "PATCH"],
     credentials: true,
   },
 });
